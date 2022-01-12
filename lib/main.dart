4d9adca9dbf07b90322f79872c1cca38f522b8b8
Sahil-107 <messisahil7@gmail.com>
2022-01-13 02:02:52 +0530

    firebase completed

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat_app/screens/welcome_screen.dart';
+import 'package:flash_chat_app/screens/login_screen.dart';
+import 'package:flash_chat_app/screens/registration_screen.dart';
+import 'package:flash_chat_app/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      // options: DefaultFirebaseOptions.currentPlatform,
+      options: FirebaseOptions(
+          apiKey: "AIzaSyCQ2mBiBGVXK001Ka_giRC_D6z-unLK7Xk",
+          appId: "1:908680520632:android:735d70008636b559f6e54d",
+          messagingSenderId: "908680520632",
+          projectId: "flash-chat-d4847"));
+  runApp(FlashChat());
+}
+
+// void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // theme: ThemeData.dark().copyWith(
+      //   textTheme: TextTheme(
+      //     bodyText1: TextStyle(color: Colors.black54),
+      //   ),
+      // ),
+      home: WelcomeScreen(),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flash_chat_app/screens/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flash_chat_app/screens/login_screen.dart';
+import 'package:flash_chat_app/screens/registration_screen.dart';
+import 'package:flash_chat_app/components/rounded_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static String id = 'welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+    animation = controller.drive(CurveTween(curve: Curves.easeIn));
+
+    // animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white.withOpacity(controller.value),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: animation.value * 100,
+                  ),
+                ),
+                Text(
+                  'Flash Chat',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            RoundedButton(
+                title: 'Log In',
+                colour: Colors.lightBlueAccent,
+                onPressed: () {
+                  Navigator.pushNamed(context, LoginScreen.id);
+                }),
+            RoundedButton(
+                title: 'Register',
+                colour: Colors.lightBlueAccent,
+                onPressed: () {
+                  Navigator.pushNamed(context, RegistrationScreen.id);
+                })
+          ],
+        ),
+      ),
+    );
+  }
+}
